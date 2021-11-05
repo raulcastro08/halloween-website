3788d068aef9ce7a2bf8cd31ffb13cc25edb6b82
raulcastro08 <raulcastro.santos5@gmail.com>
2021-11-04 23:17:49 -0300

    feat: new features added

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,7 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 let Homeswiper = new Swiper(".home-swiper", {
   spaceBetween: 30,
+
   loop: true,
   pagination: {
     el: ".swiper-pagination",
@@ -45,9 +46,61 @@ let Homeswiper = new Swiper(".home-swiper", {
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== NEW SWIPER ===============*/
+let Newswiper = new Swiper(".new-swiper", {
+  spaceBetween: 12,
+  concetredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
 /*=============== SHOW SCROLL UP ===============*/
 
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  //Quando o scroll for mais alto que 460 viewport height, adiciona class show-scroll para a tag com classe scroll-top
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  orgigin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: false,
+});
+
+sr.reveal(".home-swiper, .new-swiper, .newsletter__container ");
+sr.reveal(".category__data, .trick__container, .footer__content", {
+  interval: 100,
+});
+sr.reveal(".about__data, .discount__img", { origin: "left" });
+sr.reveal(".about__img, .discount__data", { origin: "right" });
